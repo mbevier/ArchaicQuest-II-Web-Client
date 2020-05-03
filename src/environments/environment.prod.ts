ad7054b3bff7621b3baa0ccdbcbca93d322e0c7e
Mark Bevier <markbevier@hotmail.com>
2020-05-03 11:11:17 -0700

    Updating prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  coreServicesURI: 'http://localhost:62640/api/',
-  coreHubURI: 'http://localhost:62640/Hubs/game',
-  API_BASE_PATH: 'http://localhost:62640'
+  coreServicesURI: 'http://52.154.153.21/api/',
+  coreHubURI: 'http://52.154.153.21/Hubs/game',
+  API_BASE_PATH: 'http://52.154.153.21'
 };
