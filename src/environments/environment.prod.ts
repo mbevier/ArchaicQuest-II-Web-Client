4a31db2741115b3c384059500d1def52cc4ae109
Mark Bevier <markbevier@hotmail.com>
2020-05-03 14:53:56 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   coreServicesURI: 'http://52.154.153.21/api/',
   coreHubURI: 'http://52.154.153.21/Hubs/game',
-  API_BASE_PATH: 'http://52.154.153.21'
+  API_BASE_PATH: 'http://20.37.136.211'
 };
